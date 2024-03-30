87e74aac3ef4c331a0310e22e06f0dec300159fc
Josh Wong <josdanwong@gmail.com>
2024-03-30 15:46:41 -0400

    new posting screen

--- src/app/(tabs)/new-post.tsx
+++ src/app/(tabs)/new-post.tsx
@@ -1,22 +1,68 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { Pressable, StyleSheet, TextInput } from "react-native";
+import { Text, View } from "@/components/Themed";
+import { useNavigation, useRouter } from "expo-router";
+import { useLayoutEffect, useState } from "react";
 
 export default function NewPostScreen() {
+  const [content, setContent] = useState('');
+
+  const navigation = useNavigation();
+  const router = useRouter(); 
+
+
+  const onPost = () => {
+    console.warn('Posting: ', content);
+    router.push('/(tabs)/')
+    setContent(''); 
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Pressable onPress={onPost} style={styles.postButton}>
+          <Text style={styles.postButtonText}>Submit</Text>
+        </Pressable>
+      ),
+    });
+  }, [onPost]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>New Post Screen</Text>
+      <TextInput
+        value = {content}
+        onChangeText={setContent}
+        placeholder="What do you want to talk about?"
+        style={styles.input}
+        multiline
+      />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
+  input: {
+    fontSize: 18,
+  },
+  postButton: {
+    backgroundColor: 'royalblue',
+    padding: 5,
+    paddingHorizontal: 15, 
+    borderRadius: 50,
+    marginRight: 10, 
+
+
+  },
+  postButtonText: {
+    color: 'white', 
+    fontWeight: 'bold', 
+  },
+
+
 });
