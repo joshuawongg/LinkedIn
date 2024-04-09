cae207b3b21ae7cfbba2920b12bb96e9e296e8bf
Josh Wong <josdanwong@gmail.com>
2024-04-09 19:46:20 -0400

    added search functionality

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,31 +1,60 @@
-import { Text, View, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import users from "../../assets/data/users.json";
 import UserListItem from "@/components/UserListItem";
 import { useNavigation } from "expo-router";
 import { useLayoutEffect, useState } from "react";
+import { gql, useLazyQuery, useQuery } from "@apollo/client";
+
+const query = gql`
+  query profileSearch($term: String) {
+    profileSearch(term: $term) {
+      id
+      image
+      name
+      position
+    }
+  }
+`;
 
 export default function SearchScreen() {
-    const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
-    const navigation = useNavigation()
+  const { data, loading, error } = useQuery(query, {
+    variables: { term: `%${search}%` },
+  });
 
-    
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerSearchBarOptions: {
-                placeholder: 'Search Users', 
-                onChangeText: setSearch,
-            }
-        })
-    }, [navigation]); 
+  useLayoutEffect(() => {
+    if (navigation) {
+    navigation.setOptions({
+      headerSearchBarOptions: {
+        placeholder: "Search Users",
+        onChangeText: (event) => setSearch(event.nativeEvent.text),
+      },
+    });
+  }
+  }, [navigation, setSearch]);
 
-
+  if (loading && !data?.profileSearch) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    console.log(error);
+    return <Text>Something went wrong!</Text>;
+  }
 
   return (
     <View style={{ backgroundColor: "white", flex: 1 }}>
       <FlatList
-        data={users}
+        contentContainerStyle={{ marginTop: 150 }}
+        data={data?.profileSearch || []}
         renderItem={({ item }) => <UserListItem user={item} />}
       />
     </View>
